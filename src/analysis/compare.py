97cca4c7aba45336a2cb9038d9e3ad0185403455
sharmimathur <s3mathur@ucsd.edu>
2020-11-15 16:48:29 -0800

    added code to compare different features

---
+++ src/analysis/compare.py
@@ -0,0 +1,53 @@
+import sys
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+sys.path.insert(0, '../data')
+
+from etl import get_features_labels, clean_array
+
+with open('../../config/data-params.json') as fh:
+    data_cfg = json.load(fh)
+    
+    data = get_features_labels(**data_cfg)
+    features, labels, specs, tree = data
+    
+    label_QCD = labels[:,0]
+    label_Hbb = labels[:,1]
+    
+    print(sum(label_QCD+label_Hbb)/len(label_QCD+label_Hbb))
+    
+    jet_features = tree.arrays(branches=['fj_pt', 
+                                     'fj_sdmass'],
+                          entrystop=20000,
+                          namedecode='utf-8')
+    
+    #print(jet_features)
+    print(len(label_QCD))
+    print(len(label_Hbb))
+    
+    jet_features = np.stack([jet_features[feat] for feat in ['fj_pt','fj_sdmass']],axis=1)
+    jet_features = clean_array(jet_features, specs, data_cfg['remove_mass_pt_window'])
+    
+    print(len(jet_features))
+    
+    plt.figure()
+
+    plt.hist(jet_features[:,0],weights=label_QCD,bins=np.linspace(0,4000,101),density=True,alpha=0.7,label='QCD')
+    plt.hist(jet_features[:,0],weights=label_Hbb,bins=np.linspace(0,4000,101),density=True,alpha=0.7,label='H(bb)')
+    plt.xlabel(r'Jet $p_{T}$ [GeV]')
+    plt.ylabel('Fraction of jets')
+    plt.legend()
+
+    plt.figure()
+    plt.savefig('fig1.png')
+
+    plt.hist(jet_features[:,1],weights=label_QCD,bins=np.linspace(0,300,101),density=True,alpha=0.7,label='QCD')
+    plt.hist(jet_features[:,1],weights=label_Hbb,bins=np.linspace(0,300,101),density=True,alpha=0.7,label='H(bb)')
+    plt.xlabel(r'Jet $m_{SD}$ [GeV]')
+    plt.ylabel('Fraction of jets')
+    plt.legend()
+
+    plt.show()
--- src/data/etl.py
+++ src/data/etl.py
@@ -36,15 +36,17 @@ def get_features_labels(file_name, features, spectators, labels, nlabels, remove
                                                           label_array_all['label_QCD_others'])
     label_array[:,1] = label_array_all['label_H_bb']
 
+    
+    feature_array = clean_array(feature_array, spec_array, remove_mass_pt_window)
+    label_array = clean_array(label_array, spec_array, remove_mass_pt_window)
+    # spec_array = clean_array(spec_array, spec_array, remove_mass_pt_window)
+    
+    return feature_array, label_array, spec_array, tree
+
+def clean_array(arr, spec_array,remove_mass_pt_window):
     # remove samples outside mass/pT window
     if remove_mass_pt_window:
-        feature_array = feature_array[(spec_array[:,0] > 40) & (spec_array[:,0] < 200) & (spec_array[:,1] > 300) & (spec_array[:,1] < 2000)]
-        label_array = label_array[(spec_array[:,0] > 40) & (spec_array[:,0] < 200) & (spec_array[:,1] > 300) & (spec_array[:,1] < 2000)]
-        spec_array = spec_array[(spec_array[:,0] > 40) & (spec_array[:,0] < 200) & (spec_array[:,1] > 300) & (spec_array[:,1] < 2000)]
+        arr = arr[(spec_array[:,0] > 40) & (spec_array[:,0] < 200) & (spec_array[:,1] > 300) & (spec_array[:,1] < 2000)]
     
     # remove unlabeled data
-    feature_array = feature_array[np.sum(label_array,axis=1)==1]
-    spec_array = spec_array[np.sum(label_array,axis=1)==1]
-    label_array = label_array[np.sum(label_array,axis=1)==1]
-
-    return feature_array, label_array, spec_array
+    return arr#[np.sum(arr,axis=1)==1]
